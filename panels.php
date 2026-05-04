4cf1cf7d759a6cddb7c2bc50e46b27a5c0d5102e
HappyPsychotic <coldsun1997@gmail.com>
2026-05-04 12:13:58 +0330

    fix(alireza): handle missing client stats when loading user services

--- panels.php
+++ panels.php
@@ -676,51 +676,64 @@ class ManagePanel
                 'uuid' => null
             );
         } elseif ($Get_Data_Panel['type'] == "alireza_single") {
-            $UsernameData2 = get_clinetsalireza($username, $Get_Data_Panel['name_panel']);
-            if (!is_array($UsernameData2)) {
+            $UsernameDataRaw = get_clinetsalireza($username, $Get_Data_Panel['name_panel']);
+            if (!is_array($UsernameDataRaw) || empty($UsernameDataRaw)) {
                 $Output = array(
                     'status' => 'Unsuccessful',
                     'msg' => "user not found"
                 );
-            }
-            $UsernameData = $UsernameData2[1];
-            $UsernameData2 = $UsernameData2[0];
-            $expire = $UsernameData['expiryTime'] / 1000;
-            if (!$UsernameData['id']) {
-                if (!isset($UsernameData['msg']))
-                    $UsernameData['msg'] = null;
-                $Output = array(
-                    'status' => 'Unsuccessful',
-                    'msg' => $UsernameData['msg']
-                );
             } else {
-                if ($UsernameData['enable']) {
-                    $UsernameData['enable'] = "active";
+                $clientConfig = isset($UsernameDataRaw[0]) && is_array($UsernameDataRaw[0]) ? $UsernameDataRaw[0] : [];
+                $clientStats = isset($UsernameDataRaw[1]) && is_array($UsernameDataRaw[1]) ? $UsernameDataRaw[1] : [];
+                $clientId = $clientStats['id'] ?? ($clientConfig['id'] ?? null);
+
+                if (empty($clientId)) {
+                    $errorMsg = $clientStats['msg'] ?? ($clientConfig['msg'] ?? "user not found");
+                    $Output = array(
+                        'status' => 'Unsuccessful',
+                        'msg' => $errorMsg
+                    );
                 } else {
-                    $UsernameData['enable'] = "deactivev";
+                    $enable = $clientConfig['enable'] ?? ($clientStats['enable'] ?? true);
+                    $subId = $clientConfig['subId'] ?? '';
+                    $expiryTime = $clientStats['expiryTime'] ?? ($clientConfig['expiryTime'] ?? 0);
+                    $expire = intval($expiryTime / 1000);
+                    $totalTraffic = isset($clientStats['total']) ? intval($clientStats['total']) : intval($clientConfig['totalGB'] ?? 0);
+                    $upTraffic = isset($clientStats['up']) ? intval($clientStats['up']) : 0;
+                    $downTraffic = isset($clientStats['down']) ? intval($clientStats['down']) : 0;
+
+                    if ($enable) {
+                        $status = "active";
+                    } else {
+                        $status = "deactivev";
+                    }
+
+                    $status_user = get_onlineclialireza($Get_Data_Panel['name_panel'], $username);
+                    if ($totalTraffic != 0 && ($totalTraffic - ($upTraffic + $downTraffic)) <= 0) {
+                        $status = "limited";
+                    }
+                    if ($expiryTime != 0 && $expire - time() <= 0) {
+                        $status = "expired";
+                    }
+
+                    $subscriptionUrl = rtrim($Get_Data_Panel['linksubx'], '/');
+                    if ($subId !== '') {
+                        $subscriptionUrl .= "/{$subId}";
+                    }
+
+                    $Output = array(
+                        'status' => $status,
+                        'username' => $clientConfig['email'] ?? ($clientStats['email'] ?? $username),
+                        'data_limit' => $totalTraffic,
+                        'expire' => $expire,
+                        'online_at' => $status_user,
+                        'used_traffic' => $upTraffic + $downTraffic,
+                        'links' => [$subId !== '' ? outputlink($subscriptionUrl) : null],
+                        'subscription_url' => $subscriptionUrl,
+                        'sub_updated_at' => null,
+                        'sub_last_user_agent' => null,
+                    );
                 }
-                $subId = $UsernameData2['subId'];
-                $status_user = get_onlineclialireza($Get_Data_Panel['name_panel'], $username);
-                if ((intval($UsernameData['total'])) != 0) {
-                    if ((intval($UsernameData['total']) - ($UsernameData['up'] + $UsernameData['down'])) <= 0)
-                        $UsernameData['enable'] = "limited";
-                }
-                if (intval($UsernameData['expiryTime']) != 0) {
-                    if ($expire - time() <= 0)
-                        $UsernameData['enable'] = "expired";
-                }
-                $Output = array(
-                    'status' => $UsernameData['enable'],
-                    'username' => $UsernameData['email'],
-                    'data_limit' => $UsernameData['total'],
-                    'expire' => $expire,
-                    'online_at' => $status_user,
-                    'used_traffic' => $UsernameData['up'] + $UsernameData['down'],
-                    'links' => [outputlink($Get_Data_Panel['linksubx'] . "/{$UsernameData2['subId']}")],
-                    'subscription_url' => $Get_Data_Panel['linksubx'] . "/{$UsernameData2['subId']}",
-                    'sub_updated_at' => null,
-                    'sub_last_user_agent' => null,
-                );
             }
         } elseif ($Get_Data_Panel['type'] == "WGDashboard") {
             $UsernameData = get_userwg($username, $Get_Data_Panel['name_panel']);
